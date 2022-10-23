1e4a598a956602737dd82b3d7f587657be48a45b
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-10-24 03:06:33 +0530

    let-variable-js-file

---
+++ variable-let.js
@@ -0,0 +1,11 @@
+let m = "my brother-is-ayyaz-stylo"
+console.log(m)
+m = "my name is fayyaz"
+console.log(m)
+//let m = "i am learn softwere-enginear"
+console.log(m)
+
+let name = "fayyaz-ur-rehman"
+console.log(name)
+//let name = "ayyazkhilji"
+console.log(name)
