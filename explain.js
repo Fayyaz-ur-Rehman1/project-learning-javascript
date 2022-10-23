134754ca7a4877350489837f6f6b0e388df6a232
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-10-23 23:32:13 +0530

    create-explain-file

---
+++ explain.js
@@ -0,0 +1,27 @@
+var name = "fayyaz-khilji"
+let fav = "urdu"
+const age = 17 
+console.log(name)
+console.log(fav)
+console.log(age)
+var name = "name"
+console.log(name)
+name = "name1"
+console.log(name)
+//let fav = "hindi"
+console.log(fav)
+favsub = ("science")
+console.log(favsub)
+//age = age + 1;
+console.log(age)
+const intro = ("Hii my name is " + name + " and my fevrouite sub is  " + fav + "and my age is" + age)
+console.log(intro)
+function fn(perameter1 , perameter2){
+  return perameter1 + perameter2 * 2
+}const result = fn(2 , 6)
+console.log(result)
+function emptyfn(){
+  return 1;
+}
+const result1 = emptyfn();
+console.log(result1);
