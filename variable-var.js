9aa9c4bca0c619a10f0f75baed52c790ceed16b5
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-10-24 03:05:12 +0530

    variable-js-file

---
+++ variable-var.js
@@ -0,0 +1,18 @@
+var name = "fayyaz-ur-rehman"
+console.log(name)
+var a = "my name is fayyaz-ur-rehman"
+console.log(a)
+a = "my father name is saddi-ur-rehman"
+console.log(a)
+var name = "my mother name is farida"
+console.log(name)
+var present = 100
+console.log(present)
+var c = 7
+var d = 10
+var e = (c + d)
+console.log(e)
+var f = 10
+var g = 4
+var h = (f < g)
+console.log(h)
