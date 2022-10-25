44b710cf472b946bf4f170a515eae99766b1b5d7
FAYYAZ-UR-REHMAN <111459002+Fayyaz-ur-Rehman1@users.noreply.github.com>
2022-10-25 23:08:11 +0530

    Add files via upload

---
+++ arrey.js
@@ -0,0 +1,16 @@
+{
+ let name = "fayyazkhilji"
+ const sub = "urdu"
+ var fayyaz = "fayyazkhilji"
+}
+/*console.log(name)
+console.log(sub)block-scope*/
+console.log(fayyaz)
+function emptyfn(){
+    var khan = "ur rehman-khilji"
+}
+//console.log(khan)
+const arr = [1,"str",true]
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
